89d13c118641245b92c4a8daf3807f2a0a41d496
Mateo <mateo.romero.contreras@gmail.com>
2026-08-17 18:00:26 -0300

    Initial commit

---
+++ backend/springtrainer/src/main/java/com/springtrainer/SpringtrainerApplication.java
@@ -0,0 +1,13 @@
+package com.springtrainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtrainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtrainerApplication.class, args);
+	}
+
+}
---
+++ backend/springtrainer/src/test/java/com/springtrainer/SpringtrainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtrainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtrainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
